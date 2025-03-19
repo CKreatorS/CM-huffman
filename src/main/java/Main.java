ffef54402d90017756605961ec19bc528774fbd6
cmorales <caitlyn.morales424@gmail.com>
2025-03-19 10:59:15 -0400

    main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,34 +1,58 @@
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Map.Entry;
+// import java.util.HashMap;
+// import java.util.Map;
+// import java.util.Map.Entry;
 
 public class Main {
+
     public static void main(String[] args) {
-        Huffman huffman = new Huffman("aaaaabbbbcccdde");
 
-        String encodedText = huffman.encode();
-        System.out.println(encodedText);
+        System.out.println("result: " + decorateFestival(7, ""));
 
-        huffman.printCodes();
+    }
 
-        String originalText = huffman.decode(encodedText);
-        System.out.println(originalText);
+    public static String decorateFestival(int length, String s) {
 
-        String text = "aaaabbbccd"; 
-        Map<Character, Integer> stuff = new HashMap<Character, Integer>(); 
-
-        for(int i = 0; i < text.length(); i++ ){
-            char c = text.charAt(i);
-            // Never seen character before
-            if(stuff.get(c) == null) {
-                stuff.put(c, 1);
-            } else {
-                stuff.put(c, stuff.get(c) + 1);
-            }
+        if(length == 0) {
+            return s;
+        } 
+        
+        if(length % 2 == 0){
+            s += "🌱"; 
+        } else {
+            s += "🌸"; 
         }
 
-        for(Entry<Character, Integer> entry : stuff.entrySet()) {
-            System.out.println("key:" + entry.getKey() + " value:" + entry.getValue()); 
-        }
+        System.out.println("length: " + length +  " " + s);
+
+        return decorateFestival(length - 1, s);
+
+
+
+    //     Huffman huffman = new Huffman("aaaaabbbbcccdde");
+
+    //     String encodedText = huffman.encode();
+    //     System.out.println(encodedText);
+
+    //     huffman.printCodes();
+
+    //     String originalText = huffman.decode(encodedText);
+    //     System.out.println(originalText);
+
+    //     String text = "aaaabbbccd"; 
+    //     Map<Character, Integer> stuff = new HashMap<Character, Integer>(); 
+
+    //     for(int i = 0; i < text.length(); i++ ){
+    //         char c = text.charAt(i);
+    //         // Never seen character before
+    //         if(stuff.get(c) == null) {
+    //             stuff.put(c, 1);
+    //         } else {
+    //             stuff.put(c, stuff.get(c) + 1);
+    //         }
+    //     }
+
+    //     for(Entry<Character, Integer> entry : stuff.entrySet()) {
+    //         System.out.println("key:" + entry.getKey() + " value:" + entry.getValue()); 
+    //     }
     }
 }
